032dea8393293844ddf5481b623351497ed65ac4
Jolin <jolinc.1230@gmail.com>
2025-06-05 22:01:30 +1200

    formathing

--- supabase/functions/update_profile/index.ts
+++ supabase/functions/update_profile/index.ts
@@ -1,55 +1,68 @@
-import "jsr:@supabase/functions-js/edge-runtime.d.ts"
-import {corsHeaders} from "../_shared/cors.ts";
+import "jsr:@supabase/functions-js/edge-runtime.d.ts";
+import { corsHeaders } from "../_shared/cors.ts";
 import SupabaseClient from "../_shared/supabaseClient.ts";
 
-
 Deno.serve(async (req) => {
-  if (req.method === 'OPTIONS') {
-    return new Response('ok', { headers: corsHeaders })
+  if (req.method === "OPTIONS") {
+    return new Response("ok", { headers: corsHeaders });
   }
-  
-  try{
-    const supabaseClient = SupabaseClient(req)
+
+  try {
+    const supabaseClient = SupabaseClient(req);
 
     // Retrieve calling user's auth and role for checking
-    const token = req.headers.get('Authorization')?.replace('Bearer ', '');
-    const { data: { user }, error: userError } = await supabaseClient.auth.getUser(token);
+    const token = req.headers.get("Authorization")?.replace("Bearer ", "");
+    const {
+      data: { user },
+      error: userError,
+    } = await supabaseClient.auth.getUser(token);
     if (userError || !user) {
-      return new Response(JSON.stringify({
-        error: 'Unauthorized'
-      }), {
-        status: 401, headers:{...corsHeaders}
-      });
+      return new Response(
+        JSON.stringify({
+          error: "Unauthorized",
+        }),
+        {
+          status: 401,
+          headers: { ...corsHeaders },
+        }
+      );
     }
 
     const body = await req.json();
+    if (!body) {
+      return new Response(null, {
+        status: 400,
+        headers: { ...corsHeaders, "Content-Type": "application/json" },
+      });
+    }
 
     const { data, error } = await supabaseClient
-      .from('profiles')
+      .from("profiles")
       .update(body)
-      .eq('id', user.id)
+      .eq("id", user.id)
       .select();
 
     if (error) {
-      console.error(error)
-      throw error
+      console.error(error);
+      throw error;
     }
 
-    return new Response(
-      null,
-      { headers: { "Content-Type": "application/json" }, status: 201 },
-    )
-
-  }catch (error) {
-    return new Response(JSON.stringify({
-      error: error.message
-    }), {
-      headers: {
-        ...corsHeaders,
-        'Content-Type': 'application/json'
-      },
-      status: 500
+    return new Response(null, {
+      headers: { "Content-Type": "application/json" },
+      status: 201,
     });
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        error: error.message,
+      }),
+      {
+        headers: {
+          ...corsHeaders,
+          "Content-Type": "application/json",
+        },
+        status: 500,
+      }
+    );
   }
-})
-
+});
