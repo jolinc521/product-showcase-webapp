d7ab1bf4c18cdd5822c99ac107bcea6e0645d9fd
Jolin <jolinc.1230@gmail.com>
2025-06-06 21:17:12 +1200

    add address to a user edge function

---
+++ supabase/functions/add_address/index.ts
@@ -0,0 +1,72 @@
+import "jsr:@supabase/functions-js/edge-runtime.d.ts";
+import { corsHeaders } from "../_shared/cors.ts";
+import SupabaseClient from "../_shared/supabaseClient.ts";
+
+Deno.serve(async (req) => {
+  const supabase = SupabaseClient(req);
+  if (req.method === "OPTIONS") {
+    return new Response("ok", { headers: corsHeaders });
+  }
+
+  try {
+    const supabaseClient = SupabaseClient(req);
+
+    // Retrieve calling user's auth and role for checking
+    const token = req.headers.get("Authorization")?.replace("Bearer ", "");
+    const {
+      data: { user },
+      error: userError,
+    } = await supabaseClient.auth.getUser(token);
+    if (userError || !user) {
+      return new Response(
+        JSON.stringify({
+          error: "Unauthorized",
+        }),
+        {
+          status: 401,
+          headers: { ...corsHeaders },
+        }
+      );
+    }
+
+    const body = await req.json();
+
+    // Fetch the product data
+    const addrData = {
+      user_id: user.id,
+      street: body.street || "",
+      city: body.city || "",
+      state: body.state || "",
+      postcode: body.postcode || "",
+      country: body.country || "",
+    };
+
+    const { data, error } = await supabaseClient
+      .from("address")
+      .insert([addrData])
+      .select();
+
+    if (error) {
+      console.error(error);
+      throw error;
+    }
+
+    return new Response(null, {
+      headers: { "Content-Type": "application/json" },
+      status: 201,
+    });
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        error: error.message,
+      }),
+      {
+        headers: {
+          ...corsHeaders,
+          "Content-Type": "application/json",
+        },
+        status: 500,
+      }
+    );
+  }
+});
